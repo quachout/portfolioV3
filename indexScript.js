6ed0d213d295df2594f9047943f1afcc21d95dc9
quachout <quachcw@uci.edu>
2021-03-19 00:31:55 -0700

    03/19/21 12:31am

---
+++ indexScript.js
@@ -0,0 +1,64 @@
+
+
+
+// BUTTON //////////////////////////////////////////////////////////////////
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// BUTTON //////////////////////////////////////////////////////////////////
+
+
+
+// navBar button //////////////////////////////////////////////////
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  
+  
+  burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      //links animation
+      navLinks.forEach((link, index) => {
+      
+          
+
+
+
+          if (link.style.animation){
+              link.style.animation ='';
+          }
+          else{
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index/5+.05}s`;
+          }
+      });
+
+
+      burger.classList.toggle('toggle');
+  });
+
+}
+
+
+navSlide();
+
+//flex-wrap
---
+++ script.js
@@ -0,0 +1,32 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        //links animation
+        navLinks.forEach((link, index) => {
+        
+            
+
+
+
+            if (link.style.animation){
+                link.style.animation ='';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/5+.05}s`;
+            }
+        });
+
+
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+
+navSlide();
